ac08b769ae6253afbde9753939cb426d9b9847aa
Jean <jeanbarrazatorres@gmail.com>
2026-08-13 15:40:05 -0400

    craacion de inventario y formulario.

--- test_1/home/urls.py
+++ test_1/home/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import IndexView, ContactoView, UserCreateView 
+from .views import IndexView, ContactoView, UserCreateView, ProductCreateView, InventarioView 
 
 urlpatterns = [
     path("", IndexView.as_view(), name = "home"),
     path("contacto/", ContactoView.as_view(), name = "contacto"),
-    path("usuarios/", UserCreateView.as_view(), name = "usuarios")
+    path("usuarios/", UserCreateView.as_view(), name = "usuarios"),
+    path("productos/", ProductCreateView.as_view(), name = "productos"),
+    path("inventario/", InventarioView.as_view(), name = "inventario")
 ]
